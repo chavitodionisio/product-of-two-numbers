e3e08f20c3ab09d95354a79b2c8f0e2fb1756157
chavitodionisio <61675850+chavitodionisio@users.noreply.github.com>
2020-03-05 16:30:18 +0300

    Add files via upload

---
+++ Product of two numbers.py
@@ -0,0 +1,7 @@
+#Python program that calculates difference
+
+num1 = int(input("Enter first Number:\n"))
+num2 = int(input("Enter Second number:\n"))
+answer = num1 * num2
+
+print("The product between first and second number is: ", answer)
